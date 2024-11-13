7a87cfb24d5879f4e662e012aa35cf19d43228b3
sakmt <kirimotch@gmail.com>
2024-11-13 19:38:52 +0900

    rename

---
+++ leeep-ios-sdk.xcframework/ios-arm64/LeeepIosSdk.framework/Headers/LeeepIosSdk.h
@@ -0,0 +1,18 @@
+//
+//  LeeepIosSdk.h
+//  LeeepIosSdk
+//
+//  Created by 坂本雄彦 on 2024/11/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LeeepIosSdk.
+FOUNDATION_EXPORT double LeeepIosSdkVersionNumber;
+
+//! Project version string for LeeepIosSdk.
+FOUNDATION_EXPORT const unsigned char LeeepIosSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeeepIosSdk/PublicHeader.h>
+
+
